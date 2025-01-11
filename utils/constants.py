481d5885ad95dee4ee88d9ede9b84fa4444bd8d6
Sicong E <150323043+siconge@users.noreply.github.com>
2025-01-11 01:45:14 -0800

    Add files via upload

--- utils/constants.py
+++ utils/constants.py
@@ -2,26 +2,25 @@ import configparser
 import os
 
 parser = configparser.ConfigParser()
-parser.read(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'config', 'config.conf'))
 
-APS_CLIENT_ID = parser.get('revit', 'aps_client_id')
-APS_CLIENT_SECRET = parser.get('revit', 'aps__client_secret')
-BIM_360_PROJECT_ID = parser.get('revit', 'bim_360_project_id')
-BIM_360_FOLDER_ID = parser.get('revit', 'bim_360_folder_id')
-REVIT_FILE_NAME = parser.get('revit', 'revit_file_name')
-REVIT_MODEL_GUID = parser.get('revit', 'revit_model_guid')
-APS_API_BASE_URL = parser.get('revit', 'aps_api_base_url')
+try:
+    parser.read(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'config', 'config.conf'))
+except Exception as e:
+    raise RuntimeError(f'Error reading configuration file: {e}')
 
-AWS_ACCESS_KEY_SECRET_NAME = parser.get('aws', 'aws_access_key_secret_name')
+APS_API_BASE_URL = parser.get('bim', 'aps_api_base_url')
+APS_CLIENT_CREDENTIALS_SECRET_NAME = parser.get('aps', 'aps_client_credentials_secret_name')
+BIM_360_PROJECT_ID = parser.get('bim', 'bim_360_project_id')
+BIM_360_ITEM_ID = parser.get('bim', 'bim_360_item_id')
+MODEL_VIEW_NAME = parser.get('bim', 'model_view_name')
+
+AWS_ACCESS_KEYS_SECRET_NAME = parser.get('aws', 'aws_access_keys_secret_name')
 AWS_REGION_NAME = parser.get('aws', 'aws_region_name')
-AWS_BUCKET_NAME = parser.get('aws', 'aws_bucket_name')
-AWS_FOLDER_NAME = parser.get('aws', 'aws_folder_name')
+S3_BUCKET_NAME = parser.get('aws', 's3_bucket_name')
+DATA_SOURCE_FOLDER_NAME = parser.get('aws', 'data_source_folder_name')
+GLUE_JOB_NAME = parser.get('aws', 'glue_job_name')
 
-INPUT_PATH = parser.get('file_paths', 'input_path')
 OUTPUT_PATH = parser.get('file_paths', 'output_path')
-
-DATABASE_HOST = parser.get('database', 'database_host')
-DATABASE_NAME = parser.get('database', 'database_name')
-DATABASE_PORT = parser.get('database', 'database_port')
-DATABASE_USER = parser.get('database', 'database_username')
-DATABASE_PASSWORD = parser.get('database', 'database_password')
+OUTPUT_FILENAME_PREFIX = parser.get('file_paths', 'output_filename_prefix')
+GLUE_JOB_SCRIPT_LOCAL_PATH = parser.get('file_paths', 'glue_job_script_local_path')
+GLUE_JOB_SCRIPT_S3_FOLDER = parser.get('file_paths', 'glue_job_script_s3_folder')
