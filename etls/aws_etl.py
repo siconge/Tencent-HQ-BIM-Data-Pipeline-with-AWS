1e1eca137eb61a952e15cee2adb50c219b7700b5
Sicong E <150323043+siconge@users.noreply.github.com>
2025-01-11 02:10:44 -0800

    Add files via upload

---
+++ etls/aws_etl.py
@@ -0,0 +1,143 @@
+import json
+import boto3
+from botocore.exceptions import ClientError
+from time import sleep
+
+
+def get_access_keys(secret_name:str):
+    """
+    Retrieves AWS access keys from AWS Secrets Manager.
+    """
+    client = boto3.client('secretsmanager')
+    try:
+        response = client.get_secret_value(SecretId=secret_name)
+    except ClientError as e:
+        print(f"Error retrieving secret: {e}")
+        return None, None
+    secret = json.loads(response['SecretString'])
+    return secret['AWS_ACCESS_KEY_ID'], secret['AWS_SECRET_ACCESS_KEY']
+
+
+def connect_to_s3(key_id:str, secret_key:str, region_name:str):
+    try:
+        s3 = boto3.client(
+            's3',
+            aws_access_key_id=key_id,
+            aws_secret_access_key=secret_key,
+            region_name=region_name
+        )
+        return s3
+    except ClientError as e:
+        print(f"Error connecting to S3: {e}")
+
+
+def create_bucket_if_not_exist(s3:boto3.client, bucket_name:str, region_name:str):
+    try:
+        # Check if bucket exists
+        response = s3.list_buckets()
+        bucket_exists = any(bucket['Name'] == bucket_name for bucket in response['Buckets'])
+        if not bucket_exists:
+            # Create the bucket if it doesn't exist
+            s3.create_bucket(
+                Bucket=bucket_name,
+                CreateBucketConfiguration={'LocationConstraint': region_name}
+            )
+            print(f'Bucket "{bucket_name}" created in region "{region_name}"')
+        else:
+            print(f'Bucket "{bucket_name}" already exists in region "{region_name}"')
+        s3.put_bucket_versioning(
+            Bucket=bucket_name,
+            VersioningConfiguration={'Status': 'Enabled'}
+        )
+        print(f'Bucket versioning enabled for "{bucket_name}"')
+    except ClientError as e:
+        print(f'Error checking or creating bucket "{bucket_name}": {e}')
+
+
+def create_folder_if_not_exist(s3:boto3.client, bucket_name:str, folder_name:str):
+    try:
+        # Check if a folder already exists as long as a single object is fetched, which it is sufficient to check for the existence of a folder
+        response = s3.list_objects_v2(Bucket=bucket_name, Prefix=f'{folder_name}/', MaxKeys=1)
+        if 'Contents' in response:
+            print(f'Folder "{folder_name}/" already exists in bucket "{bucket_name}"')
+        
+        # Add a zero-byte object to create the folder structure
+        else:
+            s3.put_object(Bucket=bucket_name, Key=f'{folder_name}/')
+            print(f'Folder "{folder_name}/" created in bucket "{bucket_name}"')
+    except ClientError as e:
+        print(f'Error creating folder "{folder_name}/": {e}')
+
+
+def upload_file_to_s3(s3:boto3.client, local_file_path:str, bucket_name:str, file_key:str):
+    try:
+        s3.upload_file(Filename=local_file_path, Bucket=bucket_name, Key=file_key)
+        print(f"File uploaded to s3://{bucket_name}/{file_key}")
+    
+    # Ensure the local file exists before attempting upload to S3
+    except FileNotFoundError:
+        print(f"File {local_file_path} not found")
+    except ClientError as e:
+        print(f"Error uploading file: {e}")
+
+
+def trigger_glue_job(s3:boto3.client, bucket_name:str, folder_name:str, file_name:str, glue_job_name:str):
+    """
+    Triggers an AWS Glue job if valid CSV files exist in the specified folder.
+    """
+    # Initialize Glue client
+    glue = boto3.client('glue')
+
+    # Define folder prefix and valid file suffix
+    prefix = f'{folder_name}/'
+    suffix = '.csv'
+    
+    try:
+        # List objects in the S3 bucket
+        response = s3.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
+        if 'Contents' not in response:
+            raise FileNotFoundError(f'Folder "{prefix}" does not exist')
+
+        # Categorize files
+        objects_in_folder = response['Contents']
+        csv_files = [obj['Key'] for obj in objects_in_folder if obj['Key'].endswith(suffix) and obj['Size'] > 0]
+        non_csv_files = [obj['Key'] for obj in objects_in_folder if not obj['Key'].endswith(suffix) and obj['Size'] > 0]
+
+        # Handle non-compatible files
+        if non_csv_files:
+            raise ValueError(f'Non-compatible files found in folder "{prefix}": {non_csv_files}')
+
+        # Handle no valid CSV files
+        if not csv_files:
+            raise ValueError(f'No valid CSV files found in folder "{prefix}"')
+        
+        # Trigger Glue job
+        print(f"Triggering Glue job for file: {file_name}")
+        response = glue.start_job_run(JobName=glue_job_name)
+        run_id = response['JobRunId']
+        return glue, run_id
+    
+    except ClientError as e:
+        print(f'Error triggering Glue ETL job "{glue_job_name}": {e}')
+    except Exception as e:
+        print(f"Unexpected error: {str(e)}")
+
+ 
+def monitor_glue_job_completion(glue:boto3.client, glue_job_name:str, run_id:str):
+    """
+    Monitors the completion status of an AWS Glue job.
+    """
+    while True:
+        response = glue.get_job_run(JobName=glue_job_name, RunId=run_id)
+        status = response['JobRun']['JobRunState']
+        end_time = response['JobRun'].get('CompletedOn')
+        duration = response['JobRun'].get('ExecutionTime')
+        if status in ['SUCCEEDED']:
+            print(f'Glue job "{glue_job_name}" succeeded. Status: {status}, End Time: {end_time}, Duration: {duration} seconds')
+            return status
+        
+        # Raise an error if the job fails or stops to notify Airflow of task failure
+        elif status in ['FAILED', 'STOPPED']:
+            raise RuntimeError(f'Glue job "{glue_job_name}" failed or stopped. Status: {status}, End Time: {end_time}')
+        print(f"Job still running. Current status: {status}. Retrying in 10 seconds...")
+        sleep(10)
---
+++ etls/bim_etl.py
@@ -0,0 +1,186 @@
+import os
+import json
+import requests
+import numpy as np
+import pandas as pd
+import boto3
+from botocore.exceptions import ClientError
+from datetime import datetime
+from utils.constants import APS_API_BASE_URL
+
+
+def get_client_credentials(secret_name:str):
+    """
+    Retrieves APS client credentials from AWS Secrets Manager.
+    """
+    client = boto3.client('secretsmanager')
+    try:
+        response = client.get_secret_value(SecretId=secret_name)
+    except ClientError as e:
+        print(f"Error retrieving secret: {e}")
+        return None, None
+    secret = json.loads(response['SecretString'])
+    return secret['AWS_ACCESS_KEY_ID'], secret['AWS_SECRET_ACCESS_KEY']
+
+
+def authenticate(client_id:str, client_secret:str) -> str:
+    """
+    Authenticates with Autodesk Platform Services (APS) API to obtain an access token.
+    """
+    url = f'{APS_API_BASE_URL}/authentication/v2/token'
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    data = {
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'grant_type': 'client_credentials'
+    }
+    response = requests.post(url, headers=headers, data=data)
+    response.raise_for_status()
+    return response.json().get('access_token')
+
+
+def get_file_urn(token:str, project_id:str, item_id:str) -> str:
+    """
+    Retrieves the URN of a Revit file in a BIM 360 project using its item ID.
+    """
+    url = f'{APS_API_BASE_URL}/data/v1/projects/b.{project_id}/items/{item_id}'
+
+    # Include in the subsequent requests the access token obtained during the authentication process
+    headers = {'Authorization': f'Bearer {token}'}
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    
+    # Extract URN from the item data
+    file_data = response.json().get('data')
+    if not file_data:
+        raise ValueError(f'File with item_id "{item_id}" not found')
+    urn = file_data.get('id')
+    return urn
+
+
+def get_model_guid(token:str, urn:str, model_view_name:str) -> str:
+    """
+    Retrieves the modelGuid of a specific view in the Revit file.
+    """
+    url = f'{APS_API_BASE_URL}/modelderivative/v2/designdata/{urn}/metadata'
+    headers = {'Authorization': f'Bearer {token}'}
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    
+    model_views = response.json()['data']['metadata']
+    model_guid = next((view['guid'] for view in model_views if view['name'] == model_view_name), None)
+    if not model_guid:
+        raise ValueError(f'Model view "{model_view_name}" not found.')
+    return model_guid
+
+
+def extract_param_data(token:str, urn:str, model_guid:str) -> pd.DataFrame:
+    """
+    Extracts object metadata (properties) from a Revit file, transforms and stores specified parameters.
+    """
+    url = f'{APS_API_BASE_URL}/modelderivative/v2/designdata/{urn}/metadata/{model_guid}/properties'
+    headers = {'Authorization': f'Bearer {token}'}
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+
+    # Filter curtain wall panels by family name
+    objects = response.json()['data']['collections']
+    filtered_panels = [obj for obj in objects if obj.get('name', '').lower() in ['sawtooth', 'flat']]
+    
+    # Extract specified parameters
+    params_to_extract = [
+        'unit_id', 'bldg_no', 'level', 'unit_type', 'unit_height_m', 'unit_span_m', 'alum_panel_width_m', 'alum_panel_area_sm', 'glazing_width_m', 'glazing_area_sm', 'ventilation_louver', 'rescue_window'
+    ]
+    unit_ls = []
+    
+    # Extract parameter data for each filtered panel
+    for panel in filtered_panels:
+        unit_params = {param: panel['properties'].get(param) for param in params_to_extract if 'properties' in panel}
+        unit_ls.append(unit_params)    
+    
+    if unit_ls:
+        unit_df = pd.DataFrame(unit_ls)
+    else:
+        raise ValueError("No valid curtain wall panels found")
+    return unit_df
+
+
+def transform_data(df:pd.DataFrame) -> pd.DataFrame:
+    """
+    Performs transformations on extracted BIM data, adds calculated columns, validates schema, and handles missing/unexpected fields.
+    """
+    df['facade_area_sm'] = df['alum_panel_area_sm'] + df['glazing_area_sm']
+    df['operable_area_sm'] = np.where(
+        (df['unit_type'] == 'sawtooth') & (df['alum_panel_width_m'] >= 0.4),
+        (df['alum_panel_width_m'] - 0.15) * (df['unit_height_m'] - 1.5),
+        0
+    )
+    df['unit_cost_usd'] = np.where(
+        (df['unit_type'] == 'sawtooth'),
+        df['facade_area_sm'] * 525,
+        df['facade_area_sm'] * 433
+    )
+    df['embodied_carbon_kgCO2e'] = (
+        df['alum_panel_area_sm'] * 167 + df['glazing_area_sm'] * 95 + (df['glazing_width_m'] * 3 + df['unit_height_m'] * 2) * 59
+    )
+    df = df.round({
+        'facade_area_sm':2,
+        'operable_area_sm':2,
+        'unit_cost_usd':2,
+        'embodied_carbon_kgCO2e':2
+    })
+
+    df['ventilation_louver'] = df['ventilation_louver'].astype(bool)
+    df['rescue_window'] = df['rescue_window'].astype(bool)
+
+    # Reorder columns
+    cols = df.columns.tolist()
+    cols_to_shift = ['ventilation_louver', 'rescue_window']
+    cols_to_keep = [col for col in cols if col not in cols_to_shift]
+    new_order = cols_to_keep + cols_to_shift
+    df = df[new_order]
+
+    # Validate for missing or unexpected columns
+    required_cols = [
+        'unit_id', 'bldg_no', 'level', 'unit_type', 'unit_height_m', 'unit_span_m', 'alum_panel_width_m', 'alum_panel_area_sm','glazing_width_m', 'glazing_area_sm', 'facade_area_sm', 'operable_area_sm', 'embodied_carbon_kgCO2e', 'unit_cost_usd','ventilation_louver', 'rescue_window'
+    ]
+    missing_cols = [col for col in required_cols if col not in df.columns]
+    unexpected_cols = [col for col in df.columns if col not in required_cols]
+    if missing_cols or unexpected_cols:
+        raise ValueError(
+            f"Warning: Missing columns detected: {missing_cols}"
+            f"Warning: Unexpected columns detected: {unexpected_cols}"
+        )
+
+    return df
+
+
+def generate_file_path(folder_path, prefix, date_format='%Y%m%d', extension='csv') -> str:
+    """
+    Generates a file path following the file naming protocol.
+    """
+    # Ensure folder exists before generating file path
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+    
+    date_str = datetime.now().strftime(date_format)
+    existing_files = [f for f in os.listdir(folder_path) if f.startswith(f'{prefix}_{date_str}')]
+    
+    # Incrementally number suffix for file names with the same date
+    suffix = str(len(existing_files) + 1).zfill(2)
+    
+    file_path = os.path.join(folder_path, f'{prefix}_{date_str}_{suffix}.{extension}')
+    return file_path
+
+
+def load_data_to_csv(data:pd.DataFrame, file_path:str) -> None:
+    """
+    Writes transformed data to CSV.
+    """
+    try:
+        data.to_csv(file_path, index=False)
+        print(f"Data extracted and saved successfully at {file_path}")
+    
+    # Raise errors to propagate failures to Airflow
+    except Exception as e:
+        raise RuntimeError(f"Error saving data to {file_path}: {e}")
