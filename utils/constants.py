d0d368ee0ba82db44cb1dc3787947aed44d13cd7
Sicong E <150323043+siconge@users.noreply.github.com>
2025-01-22 23:05:10 -0800

    Update constants.py

--- utils/constants.py
+++ utils/constants.py
@@ -1,12 +1,14 @@
 import configparser
 import os
+import logging
 
 parser = configparser.ConfigParser()
 
 try:
     parser.read(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'config', 'config.conf'))
 except Exception as e:
-    raise RuntimeError(f'Error reading configuration file: {e}')
+    logging.exception(f'Error reading configuration file: {e}')
+    raise
 
 APS_API_BASE_URL = parser.get('bim', 'aps_api_base_url')
 APS_CLIENT_CREDENTIALS_SECRET_NAME = parser.get('bim', 'aps_client_credentials_secret_name')
@@ -14,7 +16,8 @@ BIM_360_PROJECT_ID = parser.get('bim', 'bim_360_project_id')
 BIM_360_ITEM_ID = parser.get('bim', 'bim_360_item_id')
 MODEL_VIEW_NAME = parser.get('bim', 'model_view_name')
 
-AWS_ACCESS_KEYS_SECRET_NAME = parser.get('aws', 'aws_access_keys_secret_name')
+AWS_ACCESS_KEY_ID = parser.get('aws', 'aws_access_key_id')
+AWS_SECRET_ACCESS_KEY = parser.get('aws', 'aws_secret_access_key')
 AWS_REGION_NAME = parser.get('aws', 'aws_region_name')
 S3_BUCKET_NAME = parser.get('aws', 's3_bucket_name')
 DATA_SOURCE_FOLDER_NAME = parser.get('aws', 'data_source_folder_name')
@@ -23,4 +26,4 @@ GLUE_JOB_NAME = parser.get('aws', 'glue_job_name')
 OUTPUT_PATH = parser.get('file_paths', 'output_path')
 OUTPUT_FILENAME_PREFIX = parser.get('file_paths', 'output_filename_prefix')
 GLUE_JOB_SCRIPT_LOCAL_PATH = parser.get('file_paths', 'glue_job_script_local_path')
-GLUE_JOB_SCRIPT_S3_FOLDER = parser.get('file_paths', 'glue_job_script_s3_folder')
+GLUE_JOB_SCRIPT_S3_FOLDER_NAME = parser.get('file_paths', 'glue_job_script_s3_folder_name')
